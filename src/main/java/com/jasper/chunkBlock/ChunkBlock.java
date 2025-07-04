67fcffd91aacdd2270e93718013b870b7b14aad7
WindowsS112 <jouw-email@example.com>
2025-07-05 01:23:05 +0200

    Implement chunk saving functionality and add borders configuration

--- src/main/java/com/jasper/chunkBlock/ChunkBlock.java
+++ src/main/java/com/jasper/chunkBlock/ChunkBlock.java
@@ -2,14 +2,18 @@ package com.jasper.chunkBlock;
 
 import com.github.yannicklamprecht.worldborder.api.WorldBorderApi;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 
 public final class ChunkBlock extends JavaPlugin {
 
     private File configFile = new File(getDataFolder(), "config.yml");
+    private File borderData = new File(getDataFolder(), "borders.yml");
+
     private double cSize;
     private WorldBorderApi worldBorderApi;
 
@@ -18,8 +22,16 @@ public final class ChunkBlock extends JavaPlugin {
         if (!configFile.exists()) {
             saveDefaultConfig();
         } else {
+            try {
+                configFile.createNewFile();
+            } catch (IOException e) {
+                Bukkit.getLogger().info("Can't load file! Error.");
+                return;
+            }
             cSize = getConfig().getInt("defaultChunkSize");
         }
+
+        YamlConfiguration modifyFile = YamlConfiguration.loadConfiguration(borderData);
 
         RegisteredServiceProvider<WorldBorderApi> provider = getServer().getServicesManager().getRegistration(WorldBorderApi.class);
 
@@ -34,7 +46,7 @@ public final class ChunkBlock extends JavaPlugin {
 
         WorldBorderApi worldBorderApi = provider.getProvider();
 
-        getCommand("chunk").setExecutor(new CreateChunk(worldBorderApi, cSize));
+        getCommand("chunk").setExecutor(new CreateChunk(worldBorderApi, cSize, borderData, modifyFile));
 
 
         Bukkit.getLogger().info("[ChunkBlock] -> Has Been Started!");
--- src/main/java/com/jasper/chunkBlock/CreateChunk.java
+++ src/main/java/com/jasper/chunkBlock/CreateChunk.java
@@ -7,18 +7,26 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class CreateChunk implements CommandExecutor {
 
     private final WorldBorderApi worldBorderApi;
     private double cSize;
+    private final File borderData;
+    private final YamlConfiguration modifyFile;
 
-    public CreateChunk(WorldBorderApi worldBorderApi, double cSize) {
+    public CreateChunk(WorldBorderApi worldBorderApi, double cSize, File borderData, YamlConfiguration modifyFile) {
         this.worldBorderApi = worldBorderApi;
         this.cSize = cSize;
+        this.borderData = borderData;
+        this.modifyFile = modifyFile;
     }
 
     @Override
@@ -43,9 +51,26 @@ public class CreateChunk implements CommandExecutor {
     public void createChunk(Player player, double cSize, Location location) {
         try {
             worldBorderApi.setBorder(player, cSize, location);
+            saveChunk(player);
+
             player.sendMessage(ChatColor.GREEN + "Sucessfully created a chunk!");
         } catch (IllegalStateException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void saveChunk(Player player) {
+        modifyFile.set("Players." + player.getName() + ".borderSize", cSize);
+        modifyFile.set("Players." + player.getName() + ".x", player.getLocation().getBlockX());
+        modifyFile.set("Players." + player.getName() + ".y", player.getLocation().getBlockY());
+        modifyFile.set("Players." + player.getName() + ".z", player.getLocation().getBlockZ());
+
+
+        try {
+            modifyFile.save(borderData);
+        } catch (IOException e) {
+            Bukkit.getLogger().info("Couldn't load borderData file.");
+            return;
         }
     }
 
